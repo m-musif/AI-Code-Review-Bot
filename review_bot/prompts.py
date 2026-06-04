4eefd850750c0b9f06dfbd675c507f87a4c91063
Muhammad Musif <muhammadmusif134@gmail.com>
2026-06-04 16:53:12 +0500

    Add file-level review prompt

--- review_bot/prompts.py
+++ review_bot/prompts.py
@@ -14,3 +14,19 @@ PR Diff:
 {pr_diff}
 """
 
+
+FILE_REVIEW_PROMPT = """
+You are a senior software engineer reviewing a single changed file in a GitHub Pull Request.
+
+File name:
+{file_name}
+
+File diff:
+{file_diff}
+
+Provide a concise review with:
+1. Bugs
+2. Security issues
+3. Code quality issues
+4. Suggestions
+"""
